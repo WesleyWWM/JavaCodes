b1a79a4a1e389937c72e54f1a303dd3dd8996e3c
WesleyWWM <65779711+WesleyWWM@users.noreply.github.com>
2023-04-25 13:43:21 +0800

    Update and rename main.java to Main.java

--- Shapes/Main.java
+++ Shapes/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  *
  * @author WesleyWEN
  */
-public class Shapes {
+public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
